42fea638041cb3e30e7246bee3ef68682af761eb
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-20 13:42:18 +0100

    begin writing axios with useState at top of app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { BrowswerRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import Header from './components/Header';
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
 
+const rickMortyApi = 'https://rickandmorty.api.com/api/character';
+
+export default function App(){
+  const [characterData, setCharacterData] = useState();
+
+
+  useEffect(() => {
+    Axios.get(rickMortyApi)
+      .then(response => {
+        console.log(response);
+        setCharacterData(response.data.results);
+      })
+      .catch(error => {
+        console.log("Server Error", error);
+      });
+  });
+}, 
